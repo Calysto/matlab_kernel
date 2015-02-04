b16c6c9a10bf7170d69ea3dc8defd74fe45dbb59
Steven Silvester <steven.silvester@ieee.org>
2015-02-04 09:40:34 -0600

    Stub out plot size handling

--- matlab_kernel.py
+++ matlab_kernel.py
@@ -5,28 +5,23 @@ from pymatbridge import Matlab
 from IPython.display import Image
 
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 
 class MatlabKernel(MetaKernel):
     implementation = 'Matlab Kernel'
-    implementation_version = '1.0'
+    implementation_version = __version__,
     language = 'matlab'
-    language_version = __version__,
+    language_version = '0.1',
     banner = "Matlab Kernel"
     language_info = {
         'mimetype': 'text/x-matlab',
-        'name': 'bash',
-        # ------ If different from 'language':
-        # 'codemirror_mode': {
-        #    "version": 2,
-        #    "name": "ipython"
-        # }
-        # 'pygments_lexer': 'language',
-        # 'version'       : "x.y.z",
+        'name': 'matlab',
         'file_extension': '.m',
         'help_links': MetaKernel.help_links,
     }
+
+    _first = True
 
     def __init__(self, *args, **kwargs):
         excecutable = kwargs.pop('excecutable', 'matlab')
@@ -38,18 +33,23 @@ class MatlabKernel(MetaKernel):
         return "This is the Matlab kernel."
 
     def do_execute_direct(self, code):
-        code = code.strip()
-        if not code:
-            return
+        if self._first:
+            self._first = False
+            self.handle_plot_settings()
+
         self.log.debug('execute: %s' % code)
         resp = self._matlab.run_code(code.strip())
         self.log.debug('execute done')
         if 'stdout' not in resp['content']:
             raise ValueError(resp)
-        if 'figures' in resp['content']:
+        if ('figures' in resp['content']
+                and self.plot_settings.get('backend', None) == 'inline'):
             for fname in resp['content']['figures']:
-                im = Image(filename=fname)
-                self.Display(im)
+                try:
+                    im = Image(filename=fname)
+                    self.Display(im)
+                except Exception as e:
+                    self.Error(e)
         return resp['content']['stdout'].strip()
 
     def get_kernel_help_on(self, info, level=0, none_on_fail=False):
@@ -62,8 +62,55 @@ class MatlabKernel(MetaKernel):
         resp = self._matlab.run_code('help %s' % code)
         return resp['content'].get('stdout', '')
 
-    def repr(self, data):
-        return data
+    def handle_plot_settings(self):
+        """Handle the current plot settings"""
+        settings = self.plot_settings
+        if settings.get('format', None) is None:
+            settings.clear()
+        settings.setdefault('backend', 'inline')
+        settings.setdefault('format', 'png')
+        settings.setdefault('size', '560,420')
+
+        cmds = []
+
+        if settings['backend'] == 'inline':
+            cmds.append("set(0, 'defaultfigurevisible', 'off');")
+        else:
+            cmds.append("set(0, 'defaultfigurevisible', 'on');")
+
+        try:
+            width, height = settings['size'].split(',')
+            width, height = int(width), int(height)
+        except Exception as e:
+            self.Error(e)
+            width, height = 560, 420
+
+        cmds.append("""
+        function make_figs(figdir)
+            figHandles = get(0, 'children');
+            for fig=1:length(figHandles)
+                f = figHandles(fig);
+                p = get(f, 'position');
+                  w = %(width)s;
+                  h = %(height)s;
+                  if (p(3) > %(width)s);
+                        h = p(4) * w / p(3);
+                  end;
+                  if (p(4) > %(height)s);
+                        w = p(3) * h / p(4);
+                  end;
+                  size_fmt = sprintf('-S%%d,%%d', w, h);
+                  outfile = fullfile(figdir, ['MatlabFig', sprintf('%%03d', fig)]);
+                  print(f, outfile, '-d%(format)s', '-tight', size_fmt);
+                close(fig);
+            end;
+        endfunction;
+        """ % dict(width=width, height=height, format=settings['format']))
+
+        cmds.append("set(0, 'DefaultFigurePosition', [300, 200, %s, %s]);" %
+                    (width, height))
+
+        self.do_execute_direct('\n'.join(cmds))
 
 if __name__ == '__main__':
     from IPython.kernel.zmq.kernelapp import IPKernelApp
