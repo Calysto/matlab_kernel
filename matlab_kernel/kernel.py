039b94883c1c49176543e08990ef8ee487538bda
Ali Uneri <auneri@users.noreply.github.com>
2016-04-17 15:52:32 -0400

    Report execution errors in stderr

--- matlab_kernel/kernel.py
+++ matlab_kernel/kernel.py
@@ -49,8 +49,7 @@ class MatlabEngine(object):
         try:
             self._engine.eval(code, nargout=0, stdout=out, stderr=err)
         except (SyntaxError, MatlabExecutionError) as exc:
-            err, = exc.args
-            raise MatlabExecutionError(err.rstrip("\n"))
+            return dict(success=False, content=dict(stdout=exc.args[0]))
         return dict(success=True, content=dict(stdout=out.getvalue()))
 
 
